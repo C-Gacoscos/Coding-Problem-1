2ffc33d05ef60d22335abb32615dd09fa57a7a1b
unknown <ceedriclazaro010205@gmail.com>
2024-11-04 01:55:02 -0800

    Initial Commit

---
+++ userProfile.js
@@ -0,0 +1,11 @@
+let firstName = "Ceedric";
+let lastName = "Gacoscos";
+let fullName = firstName + " " + lastName;
+let age = 19;
+let favNumber = 69;
+let favColor = "Black";
+
+console.log(fullName);
+console.log(age);
+console.log(favNumber);
+console.log(favColor);
